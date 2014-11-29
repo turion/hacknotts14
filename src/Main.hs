88c2667c845dca278729e255a0dba1683917105a
Ivan Perez <ivan.perez@keera.co.uk>
2014-11-29 13:37:40 +0000

    Demo program, blinking light

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,13 @@
-main =  print "Hello"
+import Control.Monad
+import System.Hardware.Arduino
+
+main = withArduino False "/dev/ttyUSB0" $ do
+           let led = digital 13
+           setPinMode led OUTPUT
+           forever $ do digitalWrite led True
+                        delay 1000
+                        digitalWrite led False
+                        delay 1000 
 
 data ArduinoInput = ArduinoInput {}
 
